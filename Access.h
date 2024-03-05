0c47e75f768008ade80eeb223e30442b1578ea0b
aaron.haag <aaron.haag@ecresorts.com>
2024-03-04 22:39:32 -0600

    progress

--- Access.h
+++ Access.h
@@ -1,15 +1,38 @@
 #include <stdint.h>
-
-struct Position{
-    int x,y,z,zx,zy;
-
+#include <stdlib.h>
+/**
+ * @struct Position
+ * @brief  This struct represents the position of a cell in the overall board.
+ */
+struct Position
+{
+    int x;      /** The Row the cell is in  (1-based)*/
+    int y;      /** The Column the cell is in (1-based)*/
+    int z;      /** The Box the cell is in (1-based)*/
+    int zx;     /** The 'Row' within the box the cell is in */
+    int zy;     /** The 'Column' within the box the cell is in */
+    int zindex; /** The index number of the cell within the Box*/
 };
 
-Position GetPosition(int i){
-    Position out;
-    out.y = i%9;
-    out.x = i/9;
-    if(out.y==0){out.y=9;out.x--;}
-    
-
+/// @brief Returns the Row, Column, Box, and box coordinates of a the input cell.
+/// @param i The Index you are ascertaining the position of.
+/// @return struct Position
+/// @see Position
+struct Position GetPosition(int i)
+{
+    struct Position out;
+    out.y = (i - 1) % 9 + 1;
+    out.x = (i - 1) / 9 + 1;
+    out.z = 3 * ((out.x - 1) / 3) + ((out.y - 1) / 3) + 1;
+    out.zx = (out.x - 1) % 3;
+    out.zy = (out.x - 1) % 3;
+    out.zindex = 3 * out.zx + out.zy + 1;
+    out.zx++;
+    out.zy++;
+    return out;
 }
+int* FillMates(int* board, int* rowMates, int* colMates, int* boxMate, struct Position p){
+    
+    
+}
+
--- Main.c
+++ Main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "Debug.h"
+#include "Access.h"
+#include "Conversion.h"
+
 // Global Variables
 char input[82] = "070000043040009610800634900094052000358460020000800530080070091902100005007040802";
 uint16_t board[82] = {0};
@@ -11,16 +14,22 @@ void convert_input(char input[81])
 {
     for (int i = 1; i < 82; i++)
     {
-        board[i] = char_to_bitmask(input[i]);
+        board[i] = char_to_bitmask(input[i-1]);
     }
 }
 
-
-
-
-
+void test(int n)
+{
+    struct Position a;
+    a = GetPosition(n);
+    printf("Input: %d Row: %d Col: %d Box: %d Zx: %d Zy: %d Zindex: %d\n\n", n, a.x, a.y, a.z, a.zx, a.zy, a.zindex);
+}
 
 // Main
-int main() {
+int main()
+{
     convert_input(input);
+    test(21);
+    test(44);
+    test(68);
 }
